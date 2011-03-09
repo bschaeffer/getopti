a1502cc23709262b8bdc537d5a88c1353cb6997f
Braden Schaeffer <hello@manasto.info>
2011-03-09 00:20:38 -0600

    add license information

--- Getopti.php
+++ Getopti.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * Getopti
+ *
+ * Getopti is a command-line parsing tool for PHP
+ *
+ * @package   Getopti
+ * @author    Braden Schaeffer <hello@manasto.info>
+ * @version   0.1.0
+ * @link      http://github.com/bschaeffer/getopti
+ *
+ * @copyright Copyright (c) 2011
+ * @license   http://www.opensource.org/licenses/mit-license.html MIT
+ *
+ * @filesource
+ */
+
 // It is either a PEAR installation instance...
 if(strpos('@php_bin@', '@php_bin') === 0)
 {
--- Getopti/Parser.php
+++ Getopti/Parser.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * Getopti
+ *
+ * Getopti is a command-line parsing tool for PHP
+ *
+ * @package   Getopti
+ * @author    Braden Schaeffer <hello@manasto.info>
+ * @version   0.1.0
+ * @link      http://github.com/bschaeffer/getopti
+ *
+ * @copyright Copyright (c) 2011
+ * @license   http://www.opensource.org/licenses/mit-license.html MIT
+ *
+ * @filesource
+ */
+
 namespace Getopti;
 
 class Parser {
