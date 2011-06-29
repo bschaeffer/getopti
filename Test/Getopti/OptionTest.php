565ef436670ddc1da382a38af8ce6bee068e1680
Braden Schaeffer <hello@manasto.info>
2011-06-28 22:14:12 -0500

    add test for parsing options and option parameters

--- Test/Getopti/OptionTest.php
+++ Test/Getopti/OptionTest.php
@@ -47,6 +47,114 @@ class OptionTest extends PHPUnit_Framework_TestCase {
     $long = ($is_long) ? 'long' : NULL;
     $this->assertEquals($long, $option->long);
   }
+
+  // --------------------------------------------------------------------
+
+  public function optsProvider()
+  {
+    return array(
+      array(
+        'a',                  // opts argument
+        'a',                  // expected short opt
+        NULL,                 // expected long opt
+      ),
+      array('long',               NULL, 'long'),
+      array(array('a'),           'a', NULL),
+      array(array('long'),        NULL, 'long'),
+      array(array('a', NULL),     'a', NULL),
+      array(array(NULL, 'long'),  NULL, 'long'),
+      array(array('a', 'long'),   'a', 'long'),
+
+      // We don't throw errors here, so empty options should work
+      array(NULL,                 NULL, NULL),
+      array(array(NULL),          NULL, NULL),
+      array(array(NULL, NULL),    NULL, NULL)
+    );
+  }
+
+  /**
+   * @test
+   * @covers  Getopti\Option::parse_opts
+   *
+   * @dataProvider  optsProvider
+   */
+  public function parses_opts_correctly($opts, $expected_short, $expected_long)
+  {
+    $results = Option::parse_opts($opts);
+
+    $this->assertEquals(
+      $expected_short, $results[0],
+      'Option::parse_opts did not extract the SHORT option correctly.'
+    );
+
+    $this->assertEquals(
+      $expected_long, $results[1],
+      'Option::parse_opts did not extract the LONG option correctly.'
+    );
+  }
+  
+  /**
+   * @test
+   * @covers  Getopti\Option::parse_opts
+   */
+  public function parse_opts_returns_correct_number_of_results()
+  {
+    $results = Option::parse_opts('a');
+    
+    $this->assertEquals(
+      2, count($results),
+      'Option::parse_opts did not return exactly two (2) results.'
+    );
+  }  
+
+  // --------------------------------------------------------------------
+
+  public function paramProvider()
+  {
+    return array(
+      array(
+        'PARAM',              // param argument
+        'PARAM',              // expected pared string
+        Option::TYPE_DEFAULT, // expected parsed type
+      ),
+      array(array('PARAM'), 'PARAM', Option::TYPE_DEFAULT)
+    );
+  }
+
+  /**
+   * @test
+   * @covers  Getopti\Option::parse_params
+   *
+   * @dataProvider  paramProvider
+   */
+  public function parses_params_correctly($params, $expected_string, $expected_type)
+  {
+    $results = Option::parse_params($params);
+
+    $this->assertEquals(
+      $expected_string, $results[0],
+      'Option::parse_param did not extract the parameter STRING correctly.'
+    );
+
+    $this->assertEquals(
+      $expected_type, $results[1],
+      'Option::parse_param did not extract the parameter TYPE correctly.'
+    );
+  }
+
+  /**
+   * @test
+   * @covers  Getopti\Option::parse_params
+   */
+  public function parse_params_returns_correct_number_of_results()
+  {
+    $results = Option::parse_params('PARAM');
+    
+    $this->assertEquals(
+      2, count($results),
+      'Option::parse_param did not return exactly two (2) results.'
+    );
+  }
 }
 
 /* End of file OptionTest.php */
